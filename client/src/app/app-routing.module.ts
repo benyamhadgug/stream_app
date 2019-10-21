ae3d45ccfaffbf65984411b829cecfdcee3ba308
benyamhadgug <benyamhadgu@gmail.com>
2019-10-20 21:56:54 -0500

    final user management and before applying UI Theme

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user-management/login/login.component';
+import { RegisterComponent } from './user-management/register/register.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent}, 
+  {path: 'login', component: LoginComponent}, 
+  {path: 'register', component: RegisterComponent}, 
   {path: '**', component: HomeComponent}
 ];
 
--- client/src/app/user-management/register/register.component.ts
+++ client/src/app/user-management/register/register.component.ts
@@ -8,29 +8,38 @@ import {
 } from "@angular/forms";
 
 import { Observable } from "rxjs";
+import { MustMatch } from './must-match.validator';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  loginForm: FormGroup;
+  registerForm: FormGroup;
   submitted = false;
   constructor(private formBuilder: FormBuilder) { 
   }
 
   ngOnInit() {
-    this.loginForm = this.formBuilder.group({
+    this.registerForm = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required], 
       confirm_password: ['', Validators.required], 
-      email: ['', Validators.required], 
-      category: ['', Validators.required], 
+      email: ['', [Validators.required, Validators.email]], 
+      category: ['M', Validators.required], 
+      acceptTerms: [false, Validators.requiredTrue]
+    }, {
+      validator: MustMatch('password', 'confirm_password')
   });
   }
   onSubmit() {
     this.submitted = true;
-    console.log(this.loginForm);
+    console.log(this.registerForm);
   }
-  get f() { return this.loginForm.controls; }
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
+  }
+  get f() { return this.registerForm.controls; }
 }
