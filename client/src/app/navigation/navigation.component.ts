124a7862ef50ae95cf3edce20cd0788aa2186929
DanfanA <danfan2015@gmail.com>
2019-10-22 18:49:08 -0500

    nav

---
+++ client/src/app/navigation/navigation.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { JwtService } from '../services/jwt.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+    
+  constructor(private breakpointObserver: BreakpointObserver,  private jwtService: JwtService , private router: Router) {}
+  
+  public doLogout() {
+    this.jwtService.logout();
+    this.router.navigate(['home']);
+  }
+}
